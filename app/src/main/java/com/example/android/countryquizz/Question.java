56139f41b3d4eef935f35dd3984060f9f4bd87be
allexandrucrisan <allexandru.crisan@gmail.com>
2018-09-25 12:52:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/countryquizz/Question.java
@@ -0,0 +1,15 @@
+package com.example.android.countryquizz;
+
+public class Question {
+
+    int imageId;
+    String question =" ";
+    String answer =" ";
+
+    private Question(int imageId, String question, String answer){
+        this.imageId = imageId;
+        this.question = question;
+        this.answer = answer;
+    }
+
+}
